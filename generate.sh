f07d74732ed4c4cadee32f5d1a8e4099e1db4adc
Maxence Maireaux <maxence@maireaux.fr>
2024-11-20 16:15:12 +0100

    ✨ feat: added config.yaml and updated generate.sh with new code generation command

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i https://app.wenti.dev/api/openapi.yaml \
-    --git-user-id "wentidev" \
-    --git-repo-id "sdk-go" \
-    -g go \
-    -o /local/
+#docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
+#    -i https://app.wenti.dev/api/openapi.yaml \
+#    --git-user-id "wentidev" \
+#    --git-repo-id "sdk-go" \
+#    -g go \
+#    -o /local/
+
+go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml https://app.wenti.dev/api/openapi.yaml
