a6960f830661a77e351f291fd742926cf719bcda
Maxence Maireaux <maxence@maireaux.fr>
2024-11-20 16:05:32 +0100

    ✨ feat: added git user and repository id in generate script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,6 @@
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i https://app.wenti.dev/api/openapi.yaml \
+    --git-user-id "wentidev" \
+    --git-repo-id "sdk-go" \
     -g go \
     -o /local/
